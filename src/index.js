ff4b82f9669cfda1ae9a2f7e756f6733e017add1
Fede Di Nuzzo <Federicodinuzzo98@gmail.com>
2023-07-27 14:43:46 -0300

    acces-control

--- src/index.js
+++ src/index.js
@@ -34,6 +34,7 @@ const app = express();
 
 // Middleware para habilitar CORS
 app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://dinuzzo-frontend.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
